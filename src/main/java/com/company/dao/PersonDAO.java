a8a8f5d1b7cb1a0e27348ef8d911e955648c12d6
Valera Cidik <cidikvalera@gmail.com>
2022-08-06 12:34:34 +0300

    test CRUD app

--- src/main/java/com/company/dao/PersonDAO.java
+++ src/main/java/com/company/dao/PersonDAO.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 @Component
 public class PersonDAO {
-    private static int PEOPLE_COUNT;
+    private static int PEOPLE_COUNT = 0;
     List<Person> people;
     {
         people = new ArrayList<>();
@@ -27,5 +27,16 @@ public class PersonDAO {
         return people.stream()
                 .filter(person -> person.getId() == id)
                 .findAny().orElse(null);
+    }
+
+    public void save(Person person){
+        person.setId(++PEOPLE_COUNT);
+        people.add(person);
+    }
+
+    public void update(int id, Person person){
+        Person personForUpdate = show(id);
+
+        personForUpdate.setName(person.getName());
     }
 }
--- src/main/java/com/company/models/Person.java
+++ src/main/java/com/company/models/Person.java
@@ -24,4 +24,8 @@ public class Person {
         this.id = id;
         this.name = name;
     }
+
+    public Person() {
+
+    }
 }
